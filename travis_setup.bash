ccaf2e78cac0a468e0c1a6544a2b0c57f4128659
Duc Ha <duchatran@gmail.com>
2016-03-30 12:52:16 -0700

    Update travis_setup.bash

--- travis_setup.bash
+++ travis_setup.bash
@@ -3,4 +3,5 @@ echo $MY_PASS > /tmp/pass.txt
 wget https://s3.amazonaws.com/sumo-travisci/travisci.enc -O travisci.enc
 openssl aes-256-cbc -in travisci.enc -out /tmp/travisci.pem -pass "file:/tmp/pass.txt"  -d
 chmod 600 /tmp/travisci.pem
+cp /tmp/travisci.pem ~/.ssh/.
 cat /tmp/travisci.pem
